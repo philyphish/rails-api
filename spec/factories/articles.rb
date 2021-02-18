159df66380ea90a5c767d676ed65fffb8d1d5d2e
philyphish <philipknigh@gmail.com>
2021-02-18 14:19:22 -0600

    Init commit

---
+++ spec/factories/articles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    title { "Sample title" }
+    content { "Sample content" }
+    slug { "sample-article" }
+  end
+end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '#validations' do
+    it 'should test that factory is valid' do
+			expect(build :article).to be_valid
+		end
+
+		it 'should validate the presence of a title' do
+			article = build :article, title: ''
+			expect(article).not_to be_valid
+			expect(article.errors.messages[:title]).to include("can't be blank")
+		end
+
+     it 'should validate the presence of a content' do
+      article = build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+  	
+     it 'should validate the presence of a slug' do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+  		end
+	 
+		it 'should validate uniqueness of slug' do
+			article = create :article
+			invalid_article = build :article, slug: article.slug
+			expect(invalid_article).not_to be_valid
+    end
+
+    it 'test a number is positive' do
+      expect(1).to be_positive
+      expect(5).to be > 3
+    end
+
+    it 'test article object' do
+      article = build(:article)
+      expect(article.title).to eq('Sample title')
+    end
+
+    it 'has an invalide title' do 
+      article = build(:article, title: '')
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+	end
+end
+
